8bb882ad4955f4c9ad4f218af117c3a0059f65e0
Ariel Pablo Fones <FonesAriel@gmail.com>
2023-09-26 22:42:52 -0300

    primer commit

---
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+
+    return (
+        <AppBar sx={{ backgroundColor: "#e2e2e2" }}>
+            <Toolbar sx={{ display: "flex", justifyContent: "space-around" }}>
+                <NavLink className="navbar-link" to="/">
+                    <Typography >
+                        Home/Categorias
+                    </Typography>
+                </NavLink>
+                <NavLink className="navbar-link" to="/products">
+                    <Typography >
+                        Productos
+                    </Typography>
+                </NavLink>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
